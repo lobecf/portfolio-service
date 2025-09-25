f56609c65fcc343a3ad5713d92b2047ff71b0fcd
Charles Lobe <charleslobe@Mac.attlocal.net>
2025-09-25 13:43:19 -0400

    test

--- src/main/java/org/portfolio/service/WebConfig.java
+++ src/main/java/org/portfolio/service/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Adjust the pattern as required
-                .allowedOrigins("http://localhost:4200") // Your Angular app's URL
+                .allowedOrigins("http://localhost:4200", "https://lobecf.github.io/portfolio-website/") // Your Angular app's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
